120105ef6a9f36e31bd01e5517d28ab8f298baf5
pajarr10 <fajaarr1001@gmail.com>
2026-06-12 12:43:11 +0700

    Add files via upload

---
+++ src/serv.py
@@ -0,0 +1,13 @@
+import os
+
+def serv():
+    print("=" * 40)
+    print("         Developer : Pajar")
+    print("    GitHub : fajaarr1002-create")
+    print("=" * 40)
+
+    input("\nPress ENTER to continue...")
+    os.system("clear")
+
+if __name__ == "__main__":
+    serv()
